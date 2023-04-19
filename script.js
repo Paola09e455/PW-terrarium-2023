20807c33bffaa34f9e93e7780b13916bec317ace
Paola09e455 <119635524+Paola09e455@users.noreply.github.com>
2023-04-19 18:24:05 +0000

    agregar index

---
+++ script.js
@@ -0,0 +1,9 @@
+// Tomando control del objeto
+let plant1 = document.getElementById("plant1")
+
+//
+plant1.addEventListener("click", ()=>{
+    let name = prompt("Hola, ¿Cual es tu nombre?");
+    if(name !== "")
+        alert(`${name} please, SAVE THE PLANET!!! 🌱`);
+});

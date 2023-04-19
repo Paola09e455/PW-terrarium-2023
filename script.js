81a14e4d27f640dc077007cb6dd74254f5edaf09
Paola09e455 <119635524+Paola09e455@users.noreply.github.com>
2023-04-19 18:30:59 +0000

    cambios

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ let plant1 = document.getElementById("plant1")
 plant1.addEventListener("click", ()=>{
     let name = prompt("Hola, ¿Cual es tu nombre?");
     if(name !== "")
-        alert(`${name} please, SAVE THE PLANET!!! 🌱`);
+        alert(`${name} please, SAVE THE PLANET!!!`);
 });
